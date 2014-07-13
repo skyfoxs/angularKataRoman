463452b60ee29843999dd7800b552d563e66b0c1
Supakit Thanadittagorn <supakit@riverpark.co.th>
2014-07-13 21:41:32 +0700

    Move parse roman into roman service

--- html/js/roman/service.js
+++ html/js/roman/service.js
@@ -2,4 +2,48 @@ angular.module("myApp").service("RomanService", RomanService);
 
 function RomanService(){
 
+	var romanMask = ["I","V","X","L","C","D","M"];
+
+	this.parseRomanFrom = function(arabic){
+		var stringArrayOfNumber = splitNumberToStringArray(arabic);
+
+        var romanNumber = "";
+        
+        for (var i=stringArrayOfNumber.length-1, j=0;i >= 0;i--,j++){
+            romanNumber=findRomanDigit(stringArrayOfNumber[i],romanMask.slice(j*2, (j*2)+3))+romanNumber;
+        }
+
+        return romanNumber;
+	};
+
+	function splitNumberToStringArray(number){
+        var stringNumber = number.toString();
+        return stringNumber.split('');
+    }
+
+    function findRomanDigit(Stringnumber,romanSign){
+        var number = parseInt(Stringnumber);
+        var roman = "";
+        switch(number){
+            case 4:
+                roman = romanSign[0]+romanSign[1];
+                break;
+            case 9:
+                roman = romanSign[0]+romanSign[2];
+                break;
+            case 5:
+                roman = romanSign[1];
+                break;
+            default:
+                if (number > 5){
+                    roman = romanSign[1];
+                    number = number % 5;
+                }
+                for (var i=0;i<number;i++){
+                    roman += romanSign[0];
+                }
+                break;
+        }
+        return roman;
+    }
 }
--- spec/roman/mycontrollersSpec.js
+++ spec/roman/mycontrollersSpec.js
@@ -1,117 +1,92 @@
-'use strict';
-
-/* jasmine specs for controllers go here */
-describe('MyCtrl', function(){
-    var $scope;
+describe("RomanService", function(){
+    "use strict";
+    var service;
     beforeEach(function() {
         module("myApp");
-        inject(createController);
+        inject(createService);
     });
 
-    function createController($rootScope, $controller){
-        $scope = $rootScope.$new();
-        $controller("MainController", {$scope: $scope});
+    function createService(RomanService){
+        service = RomanService;
     };
 
-  it('should be I when input is 1', function() {
-  	$scope.arabicNumber = 1;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("I");
-  });
-  it('should be II when input is 2', function() {
-  	$scope.arabicNumber = 2;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("II");
-  });
-  it('should be III when input is 3', function() {
-  	$scope.arabicNumber = 3;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("III");
-  });
-  it('should be IV when input is 4', function() {
-  	$scope.arabicNumber = 4;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("IV");
-  });
-  it('should be V when input is 5', function() {
-  	$scope.arabicNumber = 5;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("V");
-  });
-  it('should be VI when input is 6', function() {
-  	$scope.arabicNumber = 6;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("VI");
-  });
-  it('should be VII when input is 7', function() {
-  	$scope.arabicNumber = 7;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("VII");
-  });
-  it('should be VIII when input is 8', function() {
-  	$scope.arabicNumber = 8;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("VIII");
-  });
-  it('should be IX when input is 9', function() {
-  	$scope.arabicNumber = 9;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("IX");
-  });
-  it('should be X when input is 10', function() {
-  	$scope.arabicNumber = 10;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("X");
-  });
-  it('should be XI when input is 11', function() {
-  	$scope.arabicNumber = 11;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("XI");
-  });
-  it('should be XII when input is 12', function() {
-  	$scope.arabicNumber = 12;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("XII");
-  });
-  it('should be XIII when input is 13', function() {
-  	$scope.arabicNumber = 13;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("XIII");
-  });
-  it('should be XIV when input is 14', function() {
-  	$scope.arabicNumber = 14;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("XIV");
-  });
-  it('should be XV when input is 15', function() {
-  	$scope.arabicNumber = 15;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("XV");
-  });
-  it('should be XVI when input is 16', function() {
-  	$scope.arabicNumber = 16;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("XVI");
-  });
-  it('should be XVII when input is 17', function() {
-  	$scope.arabicNumber = 17;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("XVII");
-  });
-  it('should be XVIII when input is 18', function() {
-  	$scope.arabicNumber = 18;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("XVIII");
-  });
-  it('should be XIV when input is 19', function() {
-  	$scope.arabicNumber = 19;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("XIX");
-  });
-  it('should be XX when input is 20', function() {
-  	$scope.arabicNumber = 20;
-  	$scope.findRoman($scope.arabicNumber);
-  	expect($scope.romanNumber).toBe("XX");
-  });
+    it('should be I when input is 1', function() {
+        expect(service.parseRomanFrom(1)).toBe("I");
+    });
 
+    it('should be II when input is 2', function() {
+        expect(service.parseRomanFrom(2)).toBe("II");
+    });
+
+    it('should be III when input is 3', function() {
+        expect(service.parseRomanFrom(3)).toBe("III");
+    });
+
+    it('should be IV when input is 4', function() {
+        expect(service.parseRomanFrom(4)).toBe("IV");
+    });
+
+    it('should be V when input is 5', function() {
+        expect(service.parseRomanFrom(5)).toBe("V");
+    });
+
+    it('should be VI when input is 6', function() {
+        expect(service.parseRomanFrom(6)).toBe("VI");
+    });
+
+    it('should be VII when input is 7', function() {
+        expect(service.parseRomanFrom(7)).toBe("VII");
+    });
+
+    it('should be VIII when input is 8', function() {
+        expect(service.parseRomanFrom(8)).toBe("VIII");
+    });
+
+    it('should be IX when input is 9', function() {
+        expect(service.parseRomanFrom(9)).toBe("IX");
+    });
+
+    it('should be X when input is 10', function() {
+        expect(service.parseRomanFrom(10)).toBe("X");
+    });
+
+    it('should be XI when input is 11', function() {
+        expect(service.parseRomanFrom(11)).toBe("XI");
+    });
+
+    it('should be XII when input is 12', function() {
+        expect(service.parseRomanFrom(12)).toBe("XII");
+    });
+
+    it('should be XIII when input is 13', function() {
+        expect(service.parseRomanFrom(13)).toBe("XIII");
+    });
+
+    it('should be XIV when input is 14', function() {
+        expect(service.parseRomanFrom(14)).toBe("XIV");
+    });
+
+    it('should be XV when input is 15', function() {
+        expect(service.parseRomanFrom(15)).toBe("XV");
+    });
+
+    it('should be XVI when input is 16', function() {
+        expect(service.parseRomanFrom(16)).toBe("XVI");
+    });
+
+    it('should be XVII when input is 17', function() {
+        expect(service.parseRomanFrom(17)).toBe("XVII");
+    });
+
+    it('should be XVIII when input is 18', function() {
+        expect(service.parseRomanFrom(18)).toBe("XVIII");
+    });
+
+    it('should be XIV when input is 19', function() {
+        expect(service.parseRomanFrom(19)).toBe("XIX");
+    });
+
+    it('should be XX when input is 20', function() {
+        expect(service.parseRomanFrom(20)).toBe("XX");
+    });
 });
