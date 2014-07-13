82ef9bf516c6dadb7d65149f8edf780615d8817d
Supakit Thanadittagorn <supakit@riverpark.co.th>
2014-07-13 22:10:25 +0700

    Change single quotes to double quotes

--- spec/roman/service_spec.js
+++ spec/roman/service_spec.js
@@ -10,83 +10,83 @@ describe("RomanService", function(){
         service = RomanService;
     };
 
-    it('should be I when input is 1', function() {
+    it("should be I when input is 1", function() {
         expect(service.parseRomanFrom(1)).toBe("I");
     });
 
-    it('should be II when input is 2', function() {
+    it("should be II when input is 2", function() {
         expect(service.parseRomanFrom(2)).toBe("II");
     });
 
-    it('should be III when input is 3', function() {
+    it("should be III when input is 3", function() {
         expect(service.parseRomanFrom(3)).toBe("III");
     });
 
-    it('should be IV when input is 4', function() {
+    it("should be IV when input is 4", function() {
         expect(service.parseRomanFrom(4)).toBe("IV");
     });
 
-    it('should be V when input is 5', function() {
+    it("should be V when input is 5", function() {
         expect(service.parseRomanFrom(5)).toBe("V");
     });
 
-    it('should be VI when input is 6', function() {
+    it("should be VI when input is 6", function() {
         expect(service.parseRomanFrom(6)).toBe("VI");
     });
 
-    it('should be VII when input is 7', function() {
+    it("should be VII when input is 7", function() {
         expect(service.parseRomanFrom(7)).toBe("VII");
     });
 
-    it('should be VIII when input is 8', function() {
+    it("should be VIII when input is 8", function() {
         expect(service.parseRomanFrom(8)).toBe("VIII");
     });
 
-    it('should be IX when input is 9', function() {
+    it("should be IX when input is 9", function() {
         expect(service.parseRomanFrom(9)).toBe("IX");
     });
 
-    it('should be X when input is 10', function() {
+    it("should be X when input is 10", function() {
         expect(service.parseRomanFrom(10)).toBe("X");
     });
 
-    it('should be XI when input is 11', function() {
+    it("should be XI when input is 11", function() {
         expect(service.parseRomanFrom(11)).toBe("XI");
     });
 
-    it('should be XII when input is 12', function() {
+    it("should be XII when input is 12", function() {
         expect(service.parseRomanFrom(12)).toBe("XII");
     });
 
-    it('should be XIII when input is 13', function() {
+    it("should be XIII when input is 13", function() {
         expect(service.parseRomanFrom(13)).toBe("XIII");
     });
 
-    it('should be XIV when input is 14', function() {
+    it("should be XIV when input is 14", function() {
         expect(service.parseRomanFrom(14)).toBe("XIV");
     });
 
-    it('should be XV when input is 15', function() {
+    it("should be XV when input is 15", function() {
         expect(service.parseRomanFrom(15)).toBe("XV");
     });
 
-    it('should be XVI when input is 16', function() {
+    it("should be XVI when input is 16", function() {
         expect(service.parseRomanFrom(16)).toBe("XVI");
     });
 
-    it('should be XVII when input is 17', function() {
+    it("should be XVII when input is 17", function() {
         expect(service.parseRomanFrom(17)).toBe("XVII");
     });
 
-    it('should be XVIII when input is 18', function() {
+    it("should be XVIII when input is 18", function() {
         expect(service.parseRomanFrom(18)).toBe("XVIII");
     });
 
-    it('should be XIV when input is 19', function() {
+    it("should be XIV when input is 19", function() {
         expect(service.parseRomanFrom(19)).toBe("XIX");
     });
 
-    it('should be XX when input is 20', function() {
+    it("should be XX when input is 20", function() {
         expect(service.parseRomanFrom(20)).toBe("XX");
     });
 });
