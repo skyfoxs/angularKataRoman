2b545b8431ae9b0034b39ef217622644680194e7
pallat <yod.pallat@gmail.com>
2014-07-13 16:48:16 +0700

    completed roman 1-999

---
+++ app/js/mycontrollers.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/* App Module */
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('MyCtrl', function ($scope) {
+$scope.arabicNumber  = 1;
+
+var romanMask = ["I","V","X","L","C","D","M"];
+
+$scope.findRoman = function (arabic){
+	var stringArrayOfNumber = splitNumberToStringArray(arabic);
+
+	$scope.romanNumber = "";
+	
+	for (var i=stringArrayOfNumber.length-1, j=0;i >= 0;i--,j++){
+		$scope.romanNumber=findRomanDigit(stringArrayOfNumber[i],romanMask.slice(j*2, (j*2)+3))+$scope.romanNumber;
+	}
+}
+
+$scope.testFindRoman = $scope.findRoman($scope.arabicNumber);
+
+function splitNumberToStringArray(number){
+	var stringNumber = number.toString();
+	return stringNumber.split('');
+}
+
+function findRomanDigit(Stringnumber,romanSign){
+	var number = parseInt(Stringnumber);
+	var roman = "";
+	switch(number){
+		case 4:
+			roman = romanSign[0]+romanSign[1];
+			break;
+		case 9:
+			roman = romanSign[0]+romanSign[2];
+			break;
+		case 5:
+			roman = romanSign[1];
+			break;
+		default:
+			if (number > 5){
+				roman = romanSign[1];
+				number = number % 5;
+			}
+			for (var i=0;i<number;i++){
+				roman += romanSign[0];
+			}
+			break;
+	}
+	return roman;
+}
+
+});
